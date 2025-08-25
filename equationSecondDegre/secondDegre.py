d2aba05dd1d658dc06eeb636508c84f8c460b84e
Jean JAKK <apotrekokoujean06@gmail.com>
2025-08-25 18:51:05 +0000

    Programme resolvant les équation de degrée 2

---
+++ equationSecondDegre/secondDegre.py
@@ -0,0 +1,35 @@
+from math import*
+# Message d'acceuil
+print("=== 2nd DEGREE EQUATION SOLVER ===\n")
+print("Ce programme permet de résoudre les équations du second degré !")
+
+# Saisie des données
+a = 0
+while a == 0 :
+    a = float(input("\nEntrer le coefficient de degré 2 (non nul) : "))
+b = float(input("Entrer le coefficient de degré 1 : "))
+c = float(input("Entrer le coefficient de degré nul : "))
+
+# Confirmation des données
+print(f"\nVotre équation est {a}x\u00B2 + {b}x  + {c}  ")   # code Unicode x\u00B2 pour écrire x²
+continuer = input("Appuyer sur 1 pour continuer , 0 sinon ")
+while continuer == 0 :
+    break
+else :
+    # traitement
+    delta = b**2 - 4*a*c
+    if delta < 0 :
+        racdelta = sqrt(-delta)   # Ici racdelta est en réalité la racine carrée de l'opposé de delta 
+
+        # Considérons le signe de a pour mieux gérer l' affichage
+        if a > 0 :
+            print(f"Les solutions sont: {-b / (2*a)} + {racdelta / (2*a)}i et {-b / (2*a)} - {racdelta / (2*a)}i")
+        if a < 0 :
+            print(f"Les solutions sont: {-b / (2*a)} {racdelta / (2*a)}i et {-b / (2*a)} {racdelta / (2*a)}i")
+
+    elif delta > 0 :
+        racdelta = sqrt(delta)   # Ici racdelta est en réalité la racine carrée de l'opposé de delta 
+        print(f"Les solutions sont: {(-b + racdelta )/ (2*a)} et {(-b - racdelta )/ (2*a)} ")
+
+    else :
+        print(f"La solution est : {-b / (2*a)}")
