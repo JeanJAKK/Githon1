ce9a28eed2484891231c0984e080362d05ea289c
Jean JAKK <apotrekokoujean06@gmail.com>
2025-08-25 18:49:26 +0000

    Ajout programme permettant de tracer un coeur

---
+++ tracerCoeur/coeur.py
@@ -0,0 +1,8 @@
+# Tracer un coeur sur 6 lignes et 7 colonnes 
+print()
+print(" " + "*"*2 + " " + "*"*2)
+print("*" + " "*2 + "*" + " "*2 + "*")
+print("*" + " "*5 + "*")
+print(" " + "*" + " "*3 + "*")
+print(" "*2 + "*" + " " + "*")
+print(" "*3 + "*")
